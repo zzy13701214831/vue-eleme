efab455cfb447ea9d0c3d3d663cbba09690fef1b
Nero <13701214831@163.com>
2017-10-14 22:06:29 +0800

    2017-10-14

--- sell/src/App.vue
+++ sell/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <v-header></v-header>
-    <div class="tab">
+    <v-header :seller="seller"></v-header>
+    <div class="tab border-1px">
       <div class="tab-item">
         <router-link to="/goods">商品</router-link>
       </div>
@@ -12,14 +12,30 @@
         <router-link to="/seller">商家</router-link>
       </div>
     </div>
+    
     <router-view></router-view>
   </div>
 </template>
   
-<script>
+<script type="text/ecmascript-6">
   import header from './components/header/header.vue';
 
+  const ERR_OK = 0;
+
   export default {
+    data () {
+      return {
+        seller: {}
+      };
+    },
+    created() {
+      this.$http.get('/api/seller').then((response) => {
+        response = response.body;
+        if (response.errno === ERR_OK) {
+          this.seller = response.data;
+        }
+      });
+    },
     components: {
       'v-header': header
     }
@@ -27,14 +43,22 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+@import "./common/stylus/mixin.styl"
   #app
     .tab
-      display:flex
+      display: flex
       width: 100%
       height: 40px
       line-height: 40px
+      border-1px(rgba(7, 17, 27, 0.1))
       .tab-item
-        flex:1
-        text-align:center
+        flex: 1
+        text-align: center
+        & > a
+          display: block
+          font-size: 14px
+          color: rgb(77, 85, 93)
+          &.active
+            color: rgb(240, 20, 20)
 
 </style>
--- sell/src/main.js
+++ sell/src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
 import App from './App';
 Vue.use(VueRouter);
 
@@ -9,10 +10,14 @@ import goods from './components/goods/goods';
 import ratings from './components/ratings/ratings';
 import seller from './components/seller/seller';
 
+import './common/stylus/index.styl';
+
+Vue.use(VueResource);
+
 const routes = [
-  { path: '/goods', component: goods },
-  { path: '/ratings', component: ratings },
-  { path: '/seller', component: seller }
+  { path: '/goods', component: goods, name: 'goods' },
+  { path: '/ratings', component: ratings, name: 'ratings' },
+  { path: '/seller', component: seller, name: 'seller' }
 ];
 
 Vue.config.productionTip = false;
@@ -21,13 +26,14 @@ Vue.config.productionTip = false;
 // 接着创建路由实例
 const router = new VueRouter({
   // ES6缩写语法，相当于routes:routes
-  routes
+  routes,
+  linkActiveClass: 'active'
 });
 // 最后创建vue实例并挂载
 const app = new Vue({
-  el: '#app',
   router,
   template: '<App/>',
   components: { App }
 }).$mount('#app');
 
+router.push('/goods');
